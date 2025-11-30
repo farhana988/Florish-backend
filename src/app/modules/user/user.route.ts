db6a483c65ae9962f15dd21630180bbd9a5bc194
farhana988 <farhana744255@gmail.com>
2025-11-30 19:15:20 +0600

    added file upload

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,14 +1,16 @@
 import express, { NextFunction, Request, Response } from "express";
 import { UserController } from "./user.controller";
 import { UserValidation } from "./user.validation";
+import { fileUploader } from "../../helper/fileUploader";
 
 const router = express.Router();
 
 router.post(
   "/register",
-  //   fileUploader.upload.single("file"),
+  fileUploader.upload.single("file"),
   (req: Request, res: Response, next: NextFunction) => {
-    req.body = UserValidation.createUserValidationSchema.parse(req.body
+    req.body = UserValidation.createUserValidationSchema.parse(
+      JSON.parse(req.body.data)
     );
     return UserController.createUser(req, res, next);
   }
--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,13 +1,13 @@
 import { Request } from "express";
 import { prisma } from "../../shared/prisma";
 import bcrypt from "bcryptjs";
-// import { fileUploader } from "../../helper/fileUploader";
+import { fileUploader } from "../../helper/fileUploader";
 
 const createUser = async (req: Request) => {
-  // if (req.file) {
-  //     const uploadResult = await fileUploader.uploadToCloudinary(req.file)
-  //     req.body.user.profilePhoto = uploadResult?.secure_url
-  // }
+  if (req.file) {
+    const uploadResult = await fileUploader.uploadToCloudinary(req.file);
+    req.body.profilePhoto = uploadResult?.secure_url;
+  }
 
   const hashPassword = await bcrypt.hash(req.body.password, 10);
 
@@ -17,6 +17,8 @@ const createUser = async (req: Request) => {
         name: req.body.name,
         email: req.body.email,
         password: hashPassword,
+        profilePhoto: req.body.profilePhoto || null, 
+        address: req.body.address || null,
       },
     });
     return user;
